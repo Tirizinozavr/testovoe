260475e09ce000de96440ebc1f8855cf75b7e7fa
IliaSysoev <135689511+IliaSysoev@users.noreply.github.com>
2024-02-12 01:53:10 +0400

    Done

---
+++ task1/predict.py
@@ -0,0 +1,14 @@
+from joblib import load
+import pandas as pd 
+import os 
+
+def predict_my_file():
+    estimator = load('task1\model.joblib')
+    df = pd.read_csv('test_task1.csv')
+    results = estimator.predict(df['text_employer'])
+    df['ACTION_ITEM_RESULT_PRODUCT_NAME'] = results
+    df.to_csv('result.csv')
+
+if __name__=='__main__':
+    print(os.listdir('task1'))
+    predict_my_file()
